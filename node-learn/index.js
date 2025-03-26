019ab2bad04d1096e43662e14e6d9aa9569c7e46
huige9999 <849344654@qq.com>
2025-03-26 13:00:40 +0800

    node文件练习完成

--- node-learn/index.js
+++ node-learn/index.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from "node:url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 class MyFile {
-  constructor(filename,name,ext,isFile,currentTime,updateTime) {
+  constructor(filename,name,ext,isFile,size,currentTime,updateTime) {
     this.filename = filename;
     this.name = name;
     this.ext = ext;
@@ -15,21 +15,46 @@ class MyFile {
     this.updateTime = updateTime;
   }
 
-  getChildren() {}
+  async getChildren() {
+    if(this.isFile) {
+      return [];
+    } else {
+      const res = await fs.promises.readdir(this.filename);
+      return Promise.all(res.map((item) => MyFile.getMyFile(path.resolve(this.filename,item))));
+    }
+  }
 
-  getContent(isBuffer = false) {
+  async getContent(isBuffer = false) {
+    if(this.isFile) {
+      if(isBuffer) {
+        return await fs.promises.readFile(this.filename);
+      }
+      return await fs.promises.readFile(this.filename,'utf-8');
+    } else {
+      return '';
+    }
+  }
 
+  static async getMyFile(filename) {
+    const res = await fs.promises.stat(filename);
+    const name = path.basename(filename);
+    const ext = path.extname(filename);
+    const isFile = res.isFile();
+    const size = res.size;
+    const currentTime = new Date(res.birthtimeMs);
+    const updateTime = new Date(res.mtimeMs);
+    return new MyFile(filename,name,ext,isFile,size,currentTime,updateTime);
   }
 }
 
 /**
  * 读取目录下的子文件/目录成MyFile对象
- * 说明：第一版不需要递归
  * @param {*} dest 
  * @returns {Promise<Array<MyFile>>}
  */
 async function readDir(dest) {
-   
+   const file = await MyFile.getMyFile(dest);
+   return await file.getChildren();
 }
 
 
