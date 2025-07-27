0c4ae930560913dfc89e4088b4d1e43e23fc69e5
Vichel <2117236210@qq.com>
2025-07-27 08:22:36 +0800

    删除已经学过的内容参考资料

--- refer/es6/8. 异步处理/8-2. Promise的链式调用/1.js
+++
@@ -1,6 +0,0 @@
-new Promise((resolve, reject) => {
-  reject(new Error('abc'));
-}).catch((err) => {
-  console.log('失败了！！', err);
-});
-
--- refer/es6/9. FetchApi/9-6. 文件上传/upload-server/src/upload/large/verify.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-const utils = require('./utils');
-module.exports = async (req, res) => {
-  const hash = req.query.hash;
-  let fileInfo = await utils.getFileInfo(hash);
-  res.send(fileInfo);
-};
