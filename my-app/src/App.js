0bb006fdab123430da08d455f55a2376a28da6ed
Vichel <2117236210@qq.com>
2025-07-30 08:21:51 +0800

    组件状态与数据传递 Part1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,72 @@
-// import React from "react";
-import MyForm from "./components/MyForm";
+import React from "react";
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      num: 1,
+    };
+  }
 
-  return (
-    <>
-      <h1>Welcome to My App</h1>
-      <MyForm />
-    </>
-  );
+  addNum = () => {
+    // const newNum = this.state.num + 1;
+    // this.setState({
+    //   num: newNum,
+    // });
+    // console.log(newNum);
+    // 回调函数里拿到最新的num
+    // this.setState(
+    //   {
+    //     num: this.state.num + 1,
+    //   },
+    //   () => {
+    //     console.log(this.state.num);
+    //   }
+    // );
+
+    // 连续调用3次，每次再上一次的状态基础上+1
+    this.setState(
+      (prevState) => {
+        return {
+          num: prevState.num + 1,
+        };
+      },
+      () => {
+        console.log(this.state.num);
+      }
+    );
+
+    this.setState(
+      (prevState) => {
+        return {
+          num: prevState.num + 1,
+        };
+      },
+      () => {
+        console.log(this.state.num);
+      }
+    );
+
+    this.setState(
+      (prevState) => {
+        return {
+          num: prevState.num + 1,
+        };
+      },
+      () => {
+        console.log(this.state.num);
+      }
+    );
+  };
+
+  render() {
+    return (
+      <>
+        <h1>{this.state.num}</h1>
+        <button onClick={this.addNum}>click</button>
+      </>
+    );
+  }
 }
 
 export default App;
