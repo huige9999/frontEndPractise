b9855d1962870bde6b62063580991a79d64a005b
汪元会 <849344654@qq.com>
2025-08-06 16:17:42 +0800

    表单练习完毕

--- my-app/src/components/RegistrationForm.jsx
+++ my-app/src/components/RegistrationForm.jsx
@@ -15,14 +15,15 @@ class RegistrationForm extends React.Component {
                 password: '',
                 skill: '', // 如果选中HTML CSS则值为"HTML,CSS"
                 education: '',
-                avatar: ''
-            }
+                avatar: '',
+            },
+            isAgree: false, // 是否同意协议
         }
         // 定义头像的ref
         this.avatarRef = React.createRef();
     }
     onChange = (e) => {
-        const { name, value } = e.target;
+        const { name, value, type } = e.target;
         // 如果name为skill，则需要处理多选框的情况
         if (name === 'skill') {
             // value(外界的) userForm.skill(本地的)
@@ -53,6 +54,29 @@ class RegistrationForm extends React.Component {
             return;
         }
 
+        // 如果是头像上传
+        if(type === 'file') {
+            const file = e.target.files[0];
+            this.setState((prevState) => ({
+                userForm: {
+                    ...prevState.userForm,
+                    avatar: file
+                }
+            }), () => {
+                console.log(this.state.userForm);
+            });
+            return;
+        }
+
+        // 如果name为isAgree
+        if(name === 'isAgree') {
+            this.setState({
+                isAgree: e.target.checked
+            }, () => {
+                console.log(this.state.isAgree);
+            });
+            return;
+        }
 
         this.setState(prevState => ({
             userForm: {
@@ -62,6 +86,17 @@ class RegistrationForm extends React.Component {
         }), () => {
             console.log(this.state.userForm);
         });
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault(); // 阻止默认提交行为
+        if (!this.state.isAgree) {
+            alert('请同意协议后再提交');
+            return;
+        }
+
+
+        console.log('提交表单', this.state.userForm);
     }
     render() {
         return <>
@@ -100,6 +135,17 @@ class RegistrationForm extends React.Component {
                 <label>个人简介</label>
                 <textarea name="bio" value={this.state.userForm.bio} onChange={this.onChange} placeholder='请输入个人简介'></textarea>
             </div>
+           <div className="formItem">
+                <label>头像</label>
+                <input type="file" ref={this.avatarRef} onChange={this.onChange}/>
+           </div>
+           <div className="formItem">
+               <label>是否同意协议</label>
+               <input type="checkbox" checked={this.state.isAgree} onChange={this.onChange} name="isAgree"/>
+           </div>
+           <div className="formItem">
+            <button type="submit" onClick={this.onSubmit}>提交</button>
+           </div>
         </form>
         </>
     }
